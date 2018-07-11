ce6be4f44dddba51176ca035c74612d322928175
semika <semika@semika-HP-Pavilion-Laptop-15-cc0xx>
2018-07-11 23:10:55 +0530

    added more changes

--- src/main/java/com/semika/Application.java
+++ src/main/java/com/semika/Application.java
@@ -40,3 +40,8 @@ public class Application {
 //java -jar target/myapplication-0.0.1-SNAPSHOT.jar , plain start of spring boot
 //java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -jar target/myapplication-0.0.1-SNAPSHOT.jar, start to debug the application
 // java -jar myproject-0.0.1-SNAPSHOT.jar --debug , staring spring boot application with debug log enable.
+//java --server.port=9000 -jar target/myapplication-0.0.1-SNAPSHOT.jar    , start the application with specific port
+//java -jar -Dspring.profiles.active=production demo-0.0.1-SNAPSHOT.jar,  -D parameter can be set 
+
+//java -Dspring.profiles.active=development -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -jar target/user-0.0.1-SNAPSHOT.jar --port=9000
+
